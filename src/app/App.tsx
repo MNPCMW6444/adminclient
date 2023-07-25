dbf1531956d649531fe816ef0559fe7e32a2113f
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-25 19:13:35 +0300

    awdf

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,122 +1,32 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import {
-  Button,
-  Container,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-  styled,
-} from "@mui/material";
+import { Tabs, Tab, Grid } from "@mui/material";
+import { useState } from "react";
+import AWS from "./apps/AWS";
 
-interface Instance {
-  id: string;
-  type: string;
-  state: string;
-  name: string;
-}
-
-const StartButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.success.main,
-  borderColor: theme.palette.success.main,
-  marginRight: theme.spacing(1),
-}));
-
-const StopButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.error.main,
-  borderColor: theme.palette.error.main,
-}));
-
-function App() {
-  const [instances, setInstances] = useState<Instance[]>([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:6544" + "/instances/all")
-      .then((response) => {
-        setInstances(response.data.allInstances);
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  }, []);
-
-  const startInstance = (id: string) => {
-    axios
-      .post("http://localhost:6544" + "/instances/startInstance", {
-        instanceId: id,
-      })
-      .then((response) => {
-        console.log(response.data);
-        // Refresh instances
-        axios
-          .get("http://localhost:6544" + "/instances/all")
-          .then((response) => {
-            setInstances(response.data.allInstances);
-          })
-          .catch((error) => {
-            console.error("There was an error!", error);
-          });
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  };
-
-  const stopInstance = (id: string) => {
-    axios
-      .post("http://localhost:6544" + "/instances/stopInstance", {
-        instanceId: id,
-      })
-      .then((response) => {
-        console.log(response.data);
-        // Refresh instances
-        axios
-          .get("http://localhost:6544" + "/instances/all")
-          .then((response) => {
-            setInstances(response.data.allInstances);
-          })
-          .catch((error) => {
-            console.error("There was an error!", error);
-          });
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  };
+const App = () => {
+  const [page, setPage] = useState("");
 
   return (
-    <Container>
-      <Typography variant="h4" component="h1" gutterBottom>
-        EC2 Instances
-      </Typography>
-      <List>
-        {instances?.map((instance, index) => (
-          <ListItem key={index}>
-            <ListItemText>
-              <Typography variant="h6">Instance ID: {instance.id}</Typography>
-              <Typography variant="h6">Name: {instance.name}</Typography>
-              <Typography variant="body1">Type: {instance.type}</Typography>
-              <Typography variant="body1">State: {instance.state}</Typography>
-            </ListItemText>
-            <StartButton
-              variant="outlined"
-              onClick={() => startInstance(instance.id)}
-            >
-              Start
-            </StartButton>
-            <StopButton
-              variant="outlined"
-              onClick={() => stopInstance(instance.id)}
-            >
-              Stop
-            </StopButton>
-          </ListItem>
-        ))}
-      </List>
-    </Container>
+    <Grid
+      container
+      direction="column"
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <Grid item>
+        <Tabs
+          value={page}
+          onChange={(_, newValue) => setPage(newValue)}
+          variant="scrollable"
+          scrollButtons="auto"
+          allowScrollButtonsMobile
+          style={{ flex: 1 }}
+        >
+          <Tab label="instances" value="instances" />
+          <Tab label="failean-oc" value="failean-oc" />
+        </Tabs>
+      </Grid>
+      {page === "instances" && <AWS />}
+    </Grid>
   );
-}
+};
 
 export default App;
---
+++ src/app/apps/AWS.tsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Button,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  styled,
+} from "@mui/material";
+
+interface Instance {
+  id: string;
+  type: string;
+  state: string;
+  name: string;
+}
+
+const StartButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.success.main,
+  borderColor: theme.palette.success.main,
+  marginRight: theme.spacing(1),
+}));
+
+const StopButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.error.main,
+  borderColor: theme.palette.error.main,
+}));
+
+function AWS() {
+  const [instances, setInstances] = useState<Instance[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:6544" + "/instances/all")
+      .then((response) => {
+        setInstances(response.data.instances);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }, []);
+
+  const startInstance = (id: string) => {
+    axios
+      .post("http://localhost:6544" + "/instances/startInstance", {
+        instanceId: id,
+      })
+      .then((response) => {
+        console.log(response.data);
+        // Refresh instances
+        axios
+          .get("http://localhost:6544" + "/instances/all")
+          .then((response) => {
+            setInstances(response.data.allInstances);
+          })
+          .catch((error) => {
+            console.error("There was an error!", error);
+          });
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  };
+
+  const stopInstance = (id: string) => {
+    axios
+      .post("http://localhost:6544" + "/instances/stopInstance", {
+        instanceId: id,
+      })
+      .then((response) => {
+        console.log(response.data);
+        // Refresh instances
+        axios
+          .get("http://localhost:6544" + "/instances/all")
+          .then((response) => {
+            setInstances(response.data.allInstances);
+          })
+          .catch((error) => {
+            console.error("There was an error!", error);
+          });
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  };
+
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        EC2 Instances
+      </Typography>
+      <List>
+        {instances?.map((instance, index) => (
+          <ListItem key={index}>
+            <ListItemText>
+              <Typography variant="h6">Instance ID: {instance.id}</Typography>
+              <Typography variant="h6">Name: {instance.name}</Typography>
+              <Typography variant="body1">Type: {instance.type}</Typography>
+              <Typography variant="body1">State: {instance.state}</Typography>
+            </ListItemText>
+            <StartButton
+              variant="outlined"
+              onClick={() => startInstance(instance.id)}
+            >
+              Start
+            </StartButton>
+            <StopButton
+              variant="outlined"
+              onClick={() => stopInstance(instance.id)}
+            >
+              Stop
+            </StopButton>
+          </ListItem>
+        )) || "loading..."}
+      </List>
+    </Container>
+  );
+}
+
+export default AWS;
